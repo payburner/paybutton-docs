1778303097cd5198977a308aef46c1cd2f373301
Chris Opler <chrisopler@yahoo.com>
2020-05-29 06:00:44 -0700

    react PayButton

--- examples/react-paybutton-demo/src/components/PayButtonDemo.js
+++ examples/react-paybutton-demo/src/components/PayButtonDemo.js
@@ -1,11 +1,14 @@
-import React from 'react'
-const PayButtonDemo = () => {
-    return (
-        <div className='pay-button-demo'>
-        <h1>Example Pay Button</h1>
-        <pay-button buttonid="7b2d5583-a178-434d-8048-516f784f8f92"
-                    onsettled={(purchase)=>alert(JSON.stringify(purchase))} price=".00002"/>
-    </div>
-)
+import React from 'react';
+import PayButton from '@payburner/paybutton-js-react';
+class PayButtonDemo extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (<React.Fragment>
+            <PayButton buttonid={'7b2d5583-a178-434d-8048-516f784f8f92'}
+                       price={'.00589'} onSettled={(purchase)=>alert(JSON.stringify(purchase))} />
+        </React.Fragment>);
+    }
 }
 export default PayButtonDemo
